8fa2464c0882f4331985184b601156ff3dbfbf6e
steadysamwise4 <davenportsam44@gmail.com>
2026-03-07 18:04:00 -0600

    created simple repl

--- repl.go
+++ repl.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"strings"
+	"fmt"
+	"os"
+	"bufio"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan() 
+    response := scanner.Text()
+		cleaned := cleanInput(response)
+		if len(cleaned) == 0 {
+			continue
+		}
+
+		firstWord := cleaned[0]
+
+		fmt.Printf("Your command was: %s\n", firstWord)
+		
+	}
+}
 
 func cleanInput(text string) []string {
 	trimmed := strings.TrimSpace(text)
 	lowercaseText := strings.ToLower(trimmed)
 	words := strings.Split(lowercaseText, " ")
-	fmt.Println("Original:", text)
-	fmt.Println("Lowercase:", lowercaseText)
-	fmt.Println(words)
 	return words
 }
